798c42e76c1ba6057769ce00231a914091ebd636
Cane7 <nace.malezic@gmail.com>
2024-08-14 16:39:11 +0200

    Removed the "Connect to arduino" button
    Connection will be established when "send" button will be clicked

--- script.js
+++ script.js
@@ -28,6 +28,11 @@ document
   .addEventListener("click", sendConfiguration);
 
 function sendConfiguration() {
+  console.log("Attempting to connect to Arduino...");
+  alert("Attempting to connect to Arduino...");
+  // Actual connection code would go here
+  //IF the connection was succesful --->
+
   const resolution = document.getElementById("resolution-select").value;
   const cc1 = document.getElementById("cc-1").value;
   const cc2 = document.getElementById("cc-2").value;
@@ -43,10 +48,3 @@ function sendConfiguration() {
   // Here you would send this configuration to the Arduino
   // using Web Serial API or another method
 }
-
-// Arduino Connection (only for demonstration, won't work locally)
-document.getElementById("connect-arduino").addEventListener("click", () => {
-  console.log("Attempting to connect to Arduino...");
-  alert("Attempting to connect to Arduino...");
-  // Actual connection code would go here
-});
